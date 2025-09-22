fe5582be16280b5709e457ef480ccdab2fd786e1
Sadia  Shakoor <sadishakoor39@gmail.com>
2025-09-22 20:57:54 +0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 name = st.text_input("Enter your name:")
 st.subheader(name)
-date = st.date_input('Enter your DOB: ')
-st.text(f"Your date of birth is: {date}")
+
 
 age = st.number_input("Enter your age: ")
 st.text(age)
@@ -12,4 +11,3 @@ st.text(age)
 #photo = st.camera_input('click your photo')
 #st.image(photo)
 
-st.video('https://youtu.be/g-ZNUL96uMw?si=0MrsNGL8zYJg2QuP')
